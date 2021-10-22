ab9c223192881040ee09570621ed3434634af360
DarlynNunez <darlyn.nunez27@gmail.com>
2021-10-22 23:57:53 +0800

    Inital Commit 2

---
+++ Assignment 1 OOP/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment_1_OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string title = "Assignment 1";
+            string message = "Darlyn D. Nuñez";
+            MessageBox.Show(message, title);
+        }
+    }
+}
